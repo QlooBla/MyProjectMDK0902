9940b3bff457e8466f42c096322c4c4f5b07e0a2
QlooBla <87988572+QlooBla@users.noreply.github.com>
2022-05-24 14:31:00 +0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<title>Ошибка | 404</title>
+	<meta charset="UTF-8">
+	<meta name="format-detection" content="telephone=no">
+	<!-- <style>body{opacity: 0;}</style> -->
+	<link rel="stylesheet" href="css/style.min.css?_v=20220306164343">
+	<link rel="stylesheet" href="css/style-404.css">
+	<link rel="shortcut icon" href="favicon.ico">
+	<!-- <meta name="robots" content="noindex, nofollow"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+	<div class="wrapper">
+		<header class="header">
+			<div class="header__container container-header">
+				<a href="index.html" class="header__logo"><picture><source srcset="img/logo.webp" type="image/webp"><img src="img/logo.jpg" alt="logo"></picture></a>
+				<div class="header__right-block">
+					<a href="" class="header__icon _icon-bell"></a>
+					<a href="" class="header__image">
+						<picture><source srcset="img/face/01.webp" type="image/webp"><img src="img/face/01.jpg" alt="imageFace"></picture>
+					</a>
+					<div class="header__info">
+						<div class="header__name">Попов Артём</div>
+						<a href="mailto:popov.artem@mail.ru" class="header__email">popov.artem@mail.ru</a>
+					</div>
+				</div>
+			</div>
+		</header>
+		<main class="page">
+            <div class="error-block">
+                <p class="error__text">
+                    Кажется, произошла непредвиденная ошибка
+                </p>
+                <h1 class="error__title">404</h1>
+                <p class="error__text">Страницы, на которую вы хотите перейти - не существует!</p>
+                <a href="index.html" type="button">Вернуться на главную</a>
+            </div>
+		</main>
+		<footer class="footer">
+			<div class="footer__container">
+			</div>
+		</footer>
+	</div>
+	<script src="js/app.min.js?_v=20220306164343"></script>
+</body>
+
+</html>
